12743581a138aa59091a1a1b59fbca89e7acb378
ricardohuggins <rhugg99@hotmail.com>
2021-02-13 19:42:21 -0500

    commit tree 861ce89144b99d77599507d482f85cdd645a4df8

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+    start = 2
+    if n > 1
+      range = (start..n-1).to_a
+      range.none? do |num|
+        n % num == 0
+      end
+    else
+      false
+    end
+  end
+  
